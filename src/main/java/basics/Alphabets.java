f4189f372292d59124ebdb45b420c38a063b7e5f
pratikshay99 <pratikshayadav1405@gmail.com>
2024-12-05 22:09:03 +0530

    Alphabets Display

---
+++ src/main/java/basics/Alphabets.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Alphabets {
+    public static void main(String a[]){
+        for(char ch='a'; ch<='z'; ch++){
+            System.out.print(ch);
+        }
+        System.out.println();
+        for(int ch=65; ch<=90; ch++){
+            System.out.print((char)ch);
+        }
+    }
+}
