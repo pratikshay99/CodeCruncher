26e5e2647314caa1dfcbcc1e81fdddb93fca55bd
pratikshay99 <pratikshayadav1405@gmail.com>
2025-03-30 22:43:42 +0530

    100 java: SubstringsOfAString another way

--- src/main/java/basics/SubstringsOfAString.java
+++ src/main/java/basics/SubstringsOfAString.java
@@ -8,16 +8,32 @@ public class SubstringsOfAString {
         System.out.println("Enter a string to print it's all substrings");
         String s = sc.next();
 
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i + 1; j <= s.length(); j++) {
-                System.out.println(s.substring(i, j));
-            }
-        }
+//        for (int i = 0; i < s.length(); i++) {
+//            for (int j = i + 1; j <= s.length(); j++) {
+//                System.out.println(s.substring(i, j));
+//            }
+//        }
         /*or inner loop can also be
          for (int j = i; j < n; j++) {
             System.out.println(str.substring(i, j + 1));
           }
         * */
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                System.out.println(s.substring(i, j + 1));
+            }
+
+        }
 
     }
 }
+/*
+ip: abc
+Output:
+        a
+        ab
+        abc
+        b
+        bc
+        c
+*/
