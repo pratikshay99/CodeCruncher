4579bd63a92848e07580810ae0d673ef82de4569
pratikshay99 <pratikshayadav1405@gmail.com>
2025-03-30 20:48:01 +0530

    100 java: Floyd'sTriangle

---
+++ src/main/java/basics/FloydsTriangle.java
@@ -0,0 +1,22 @@
+package basics;
+
+public class FloydsTriangle {
+    public static void main(String[] args) {
+        System.out.println("Number of rows you want is n=5");
+        int n = 5;
+        int c = 1;
+        //char c = 'A'; for alphabet---> c++
+        for (int i = 1; i < n; i++) {       //row
+            for (int j = 1; j <= i; j++) {         //column
+                System.out.print(c++);
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+*/
