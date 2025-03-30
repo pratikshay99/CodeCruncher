95b95cb364fe8a3a3eb2744d3b99803d3c59a94b
pratikshay99 <pratikshayadav1405@gmail.com>
2025-03-30 22:57:55 +0530

    100 java: ReverseString with & without inbuilt func

---
+++ src/main/java/basics/ReverseString.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String s = "abc";
+        StringBuilder sb = new StringBuilder(s);
+
+        System.out.println("1. Using stringBuilder inbuilt method: " + sb.reverse());
+
+        System.out.print("2. Without inbuilt method:");
+        for (int i = (s.length() - 1); i >= 0; i--) {
+            System.out.print(s.charAt(i));
+        }
+    }
+}
+/*
+1. Using stringBuilder inbuilt method: cba
+2. Without inbuilt method:cba
+*/
