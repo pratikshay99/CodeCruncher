368abe53695b35c1b936fc20b158a7092bf18f75
pratikshay99 <pratikshayadav1405@gmail.com>
2025-03-31 23:33:49 +0530

    100 java: StringComparison: ==, equals(), equalsIgnoreCase(), compareTo()

---
+++ src/main/java/basics/Strings/StringEndwith.java
@@ -0,0 +1,12 @@
+package basics.Strings;
+
+public class StringEndwith {
+    public static void main(String[] args) {
+        String s1 = "java by TechnoLamror";
+        System.out.println(s1.endsWith("r"));           //true
+        System.out.println(s1.endsWith("Lamror"));      //true
+        System.out.println(s1.endsWith("lamror"));      //false
+
+    }
+}
+//string width with specific char
