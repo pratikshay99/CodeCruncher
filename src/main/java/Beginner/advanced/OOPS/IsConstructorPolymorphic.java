6dbf448ca2b6329dd6872b9428c28c260cf274c8
pratikshay99 <pratikshayadav1405@gmail.com>
2025-04-06 07:39:23 +0530

    Is Constructor Polymorphic? Constructor can override or overload?

---
+++ src/main/java/Beginner/advanced/OOPS/IsConstructorPolymorphic.java
@@ -0,0 +1,36 @@
+package Beginner.advanced.OOPS;
+
+class Parent {
+    Parent() {
+        System.out.println("Parent constructor");
+    }
+}
+
+class Child extends Parent {
+    Child() {
+        System.out.println("Child constructor");
+    }
+}
+
+public class IsConstructorPolymorphic {
+    public static void main(String[] args) {
+        Parent obj = new Child(); // which constructor runs?
+        //But this is not polymorphism. Java is not calling the constructor based on obj’s type — it simply runs both constructors during object creation.
+    }
+    /*Output:
+        Parent constructor
+        Child constructor*/
+}
+/*
+🚫 Constructors Can’t Be Overridden — That’s Why They’re Not Polymorphic
+Because constructors are not inherited, and overriding requires inheritance + same method signature + different behavior.
+Resolved at	-> method for Runtime (for overridden) -> constructor at Compile-time
+“Constructors support overloading but not overriding, so they are not polymorphic.”
+We mean:
+-You can have multiple constructors in a class (overloading ✅)
+-But you cannot override a constructor in a subclass — no runtime polymorphism there ❌
+
+✅ Overriding = Polymorphism
+❌ Constructors can’t be overridden
+➡️ Therefore, constructors ≠ polymorphic
+ */
