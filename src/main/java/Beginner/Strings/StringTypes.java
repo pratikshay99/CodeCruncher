9266f1b9d07f4e9242530f7c7043ae3a6484e1a7
pratikshay99 <pratikshayadav1405@gmail.com>
2025-04-10 01:06:38 +0530

    Can We Use final With String

--- src/main/java/Beginner/Strings/StringTypes.java
+++ src/main/java/Beginner/Strings/StringTypes.java
@@ -1,5 +1,8 @@
 package Beginner.Strings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringTypes {
     public static void main(String[] args) {
         String a = "Hello";
@@ -11,9 +14,10 @@ public class StringTypes {
 
 
         // String (immutable, thread safe bcz of immutable)
-        String s = "Hello";
+        String s = "Hiii";
+        s = "Hello";
         s.concat(" World"); // returns new object, not saved
-        System.out.println(s); // Hello ❌
+        System.out.println(s); // Hello
 
         // StringBuffer (mutable, thread-safe in case of synchronization)
         StringBuffer sb = new StringBuffer("Hello");
@@ -23,11 +27,48 @@ public class StringTypes {
         // StringBuilder (mutable, faster, not thread-safe)
         StringBuilder sb2 = new StringBuilder("Hello");
         sb2.append(" Java");
+        sb2 = new StringBuilder("Python"); //allow
         System.out.println(sb2); // Hello Java ✅
+        /** ❓Can We Use final With String?
+                Yes, it prevents reference reassignment but does not prevent modification (for mutable objects)
+         **/
+
+    /**
+        final String name = "Java";
+        name = "Python"; // Compilation error
+
+    */
+
+    /**
+        final StringBuilder sb1 = new StringBuilder("Java");
+        sb1.append(" Developer"); // ✅ Allowed: modifying the object
+        System.out.println(sb); // Java Developer
+
+        sb1 = new StringBuilder("Python"); // ❌ Compilation Error! (final) prevents reference reassignment
+    */
+
+    /**
+        final List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
+        System.out.println(list); // [Java, Python]
+
+        list = new ArrayList<>(); // ❌ Compilation error (reference can't be reassigned)
+    */
     }
 }
 /**
- String       → Immutable, safe but slow for many changes
- StringBuffer → Mutable, thread-safe
- StringBuilder → Mutable, fast, not thread-safe
+ * String       → Immutable, safe but slow for many changes
+ * StringBuffer → Mutable, thread-safe
+ * StringBuilder → Mutable, fast, not thread-safe
+ */
+
+/**
+  sb.append(...)	            ✅ Yes	You're modifying the object — and StringBuilder is mutable
+  sb = new StringBuilder(...)	❌ No	sb is final, so you can’t reassign the reference
+ *
+ 📌 Key Concept:
+     final → locks the reference, not the contents
+     StringBuilder → is mutable, so we can modify its content
+     But we can't reassign the final variable to a new StringBuilder object
  */
