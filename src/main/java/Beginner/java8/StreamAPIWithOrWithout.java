4a54177e58366fac48616e9ced5809fcc2833149
pratikshay99 <pratikshayadav1405@gmail.com>
2025-04-12 13:50:30 +0530

    Stream Methods commonly used

--- src/main/java/Beginner/java8/StreamAPIWithOrWithout.java
+++ src/main/java/Beginner/java8/StreamAPIWithOrWithout.java
@@ -7,7 +7,15 @@ import java.util.stream.Collectors;
 /// ❓ From a list of integers, get even numbers, square them, and collect into a new list.
 public class StreamAPIWithOrWithout {
     public static void main(String[] args) {
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> numbers = List.of(3, 2, 1, 4, 5, 6);
+
+        System.out.println(numbers.stream().sorted().collect(Collectors.toList())); //eeuivalent: numbers.stream().sorted().toList()
+        //[1, 2, 3, 4, 5, 6]
+
+        System.out.println(numbers.stream().limit(3).toList()); // First 3 items
+        System.out.println(numbers.stream().skip(2).toList());  // Skip first 2
+
+
         List<Integer> evenSquares = new ArrayList<>();
 
         //🚫 Without Stream API (Traditional Approach)
@@ -25,6 +33,17 @@ public class StreamAPIWithOrWithout {
                 .collect(Collectors.toList());
 
         System.out.println(evenSquaresS);
+
+    /// ❓Get square of even numbers from list and return a comma-separated String.
+        List<Integer> num = List.of(1, 2, 3, 4, 5, 6);
+
+        String result = num.stream()
+                .filter(n -> n % 2 == 0)              // 2, 4, 6
+                .map(n -> n * n)                      // 4, 16, 36
+                .map(String::valueOf)                // "4", "16", "36"
+                .collect(Collectors.joining(", "));  // "4, 16, 36"
+
+        System.out.println(result);
     }
 }
 /**
